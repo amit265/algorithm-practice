911586bbfc76e3185a76df27ac82c3ca8ff411fa
Amit kumar <amitk.kumar414@gmail.com>
2025-12-17 11:09:50 +0530

    array reverse

---
+++ dsa/arrays/reverse_array.js
@@ -0,0 +1,23 @@
+//  reverse an array in place
+
+
+// using two poiter
+// one starting at beginning and other at the end
+
+function reverseArray(arr){
+    if(!Array.isArray(arr)) throw new TypeError("Input must be an array");
+    if(arr.length === 0) return 0;
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while(left < right) {
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        left++;
+        right--
+    }
+
+    return arr;
+}
+
+console.log(reverseArray([1,2,3,4,5]));
