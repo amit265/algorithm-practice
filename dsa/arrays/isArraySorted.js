2825dc33cb9bbb0dc167b211cec1e7b29766650a
Amit kumar <amitk.kumar414@gmail.com>
2026-01-02 20:56:14 +0530

    isArraySorted() added

---
+++ dsa/arrays/isArraySorted.js
@@ -0,0 +1,23 @@
+
+
+
+function isArraySorted(arr) {
+    if(!Array.isArray(arr)) throw new TypeError("Input must be an array");
+    for(let i = 1; i < arr.length; i++) {
+        if(arr [i] < arr [i - 1]) return false;
+    }
+
+    return true;
+}    
+
+console.log(isArraySorted([1, 2, 3, 4, 5])); // true
+
+console.log(isArraySorted([5, 3, 4, 1, 2])); // false
+
+console.log(isArraySorted([])); // true
+
+console.log(isArraySorted([10])); // true
+
+console.log(isArraySorted([1, 2, 2, 3, 4])); // true
+
+console.log(isArraySorted([1, 3, 2, 4, 5])); // false
