b68790994567cc1ed532899090f72f9d87c199a6
Amit kumar <amitk.kumar414@gmail.com>
2026-01-06 01:34:18 +0530

    minStack.js

---
+++ Recursion, Stack & Queue/minStack.js
@@ -0,0 +1,48 @@
+// Stack that returns minimum in O(1).
+
+class MinStack {
+  constructor() {
+    this.stack = [];
+    this.minStack = [];
+  }
+
+  push(val) {
+    this.stack.push(val);
+    if (
+      this.minStack.length === 0 ||
+      val <= this.minStack[this.minStack.length - 1]
+    ) {
+      this.minStack.push(val);
+    }
+  }
+
+  pop() {
+    const val = this.stack.pop();
+    if (val === this.minStack[this.minStack.length - 1]) {
+      this.minStack.pop();
+    }
+    return val;
+  }
+
+  top() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  getMin() {
+    return this.minStack[this.minStack.length - 1];
+  }
+}
+
+// time complexity: O(1) for all operations
+// space complexity: O(n) for the stacks
+
+// test the MinStack class
+
+const minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+console.log(minStack.getMin()); // Output: -3
+minStack.pop();
+console.log(minStack.top());    // Output: 0
+console.log(minStack.getMin()); // Output: -2   
