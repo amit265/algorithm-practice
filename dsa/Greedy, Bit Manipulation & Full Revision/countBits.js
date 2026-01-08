4d6f32d624597004feea2f590ec6af5c67f102d7
Amit kumar <amitk.kumar414@gmail.com>
2026-01-09 01:56:36 +0530

    countBits.js

---
+++ dsa/Greedy, Bit Manipulation & Full Revision/countBits.js
@@ -0,0 +1,12 @@
+// This is Brian Kernighan’s trick.
+
+function countBits(n) {
+  let count = 0;
+  while (n) {
+    n = n & (n - 1);
+    count++;
+  }
+  return count;
+}
+
+
---
+++ dsa/Greedy, Bit Manipulation & Full Revision/isPowerOfTwo.js
@@ -0,0 +1,10 @@
+//Part B — Bit Manipulation (Precision Tools)
+
+
+
+function isPowerOfTwo(n) {
+  return n > 0 && (n & (n - 1)) === 0;
+}
+
+
+console.log(isPowerOfTwo(64));
