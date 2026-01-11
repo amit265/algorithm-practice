20a21ece3293957e8a3382251b5a7c1110cc76bc
Amit kumar <amitk.kumar414@gmail.com>
2026-01-12 02:48:46 +0530

    general value

--- project-euler/phase-1/1-10/specialPythagoreanTriplet.js
+++ project-euler/phase-1/1-10/specialPythagoreanTriplet.js
@@ -1,8 +1,8 @@
 function specialPythagoreanTriplet(n) {
  let sumOfabc = n;
-  for(let a = 1; a < 333; a++) {
-    for(let b = a + 1; b < 500; b++){
-      const c = 1000 - a - b;
+  for(let a = 1; a < n; a++) {
+    for(let b = a + 1; b < n; b++){
+      const c = n - a - b;
       if(a*a + b*b === c*c) {
         sumOfabc = a * b *c;
       }
