d85051c6cfc61b871a59928a2eb1300ec61b0ebe
Amit kumar <amitk.kumar414@gmail.com>
2026-01-24 02:46:01 +0530

    addition.js

---
+++ javascript/1_basic-programs/addTwoNumber.js
@@ -0,0 +1,22 @@
+
+// function to add two number
+
+function addition(a, b){
+    return a + b;
+}
+
+console.log(addition(4,5))
+
+
+// function addition(a, b){
+//     return a += b;
+// }
+
+// console.log(addition(4,5))
+
+
+
+
+
+// let addition = (a, b) => a + b;
+
