3f7c8eb45a33909d230aac8d04599d03be7e2a55
Amit kumar <amitk.kumar414@gmail.com>
2026-01-24 02:49:20 +0530

    multiplication

---
+++ javascript/1_basic-programs/multiply.js
@@ -0,0 +1,20 @@
+// multiplication of two number
+
+
+// Creating first variable
+let a = 10;
+
+// Creating second variable
+let b = 10;
+let result = a * b;
+
+// Printing the result
+// in console
+console.log("Result :", result);
+
+
+
+
+// const mulitply = (a, b) => a * b;
+
+// console.log(mulitply(5,4))
