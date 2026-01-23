98cc5871b4af435ce797a7ddb3b59d8da38c5653
Amit kumar <amitk.kumar414@gmail.com>
2026-01-24 03:31:43 +0530

    sumOfnaturalnumber.js

--- javascript/1_basic-programs/sumOfNaturalNumber.js
+++ javascript/1_basic-programs/sumOfNaturalNumber.js
@@ -1,2 +1,34 @@
 // 7. Find the Sum of Natural Numbers
 
+// using loop
+
+function findSumUsingLoop(n) {
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(findSumUsingLoop(10));
+
+// using recursion
+
+function findSumUsingRecursion(n) {
+  if (n !== 0) return n + findSumUsingRecursion(n - 1);
+  else return n;
+
+  // if(n === 1) return 1;
+  // if(n > 1) return n + findSumUsingRecursion(n - 1);
+}
+
+console.log(findSumUsingRecursion(10));
+
+
+// using maths formula 
+
+function findSumUsingMaths(n) {
+    return n * (n +  1)/2
+}
+
+console.log(findSumUsingMaths(10))
