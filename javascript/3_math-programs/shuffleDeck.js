a69fe9a65164690be729dce7894af990f6495dee
Amit kumar <amitk.kumar414@gmail.com>
2026-01-27 01:09:43 +0530

    shuffleDeck.js

---
+++ javascript/3_math-programs/shuffleDeck.js
@@ -0,0 +1,74 @@
+// 10. Shuffle Deck of Cards
+
+//Using Fisher–Yates shuffle Algorithm method
+
+function shuffleDeck() {
+  let cards = ["Hearts", "Diamonds", "Clubs", "Spades"];
+  let values = [
+    "Ace",
+    "King",
+    "Queen",
+    "Jack",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+  ];
+
+  let res = [];
+
+  for (let card in cards) {
+    for (let value in values) {
+      res.push(values[value] + " of " + cards[card]);
+    }
+  }
+
+  for (let i = res.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [res[i], res[j]] = [res[j], res[i]];
+  }
+
+  return res;
+}
+
+// console.log(shuffleDeck());
+
+// Using sorting and a random number generator
+
+function shuffleCard() {
+  let cards = ["Hearts", "Diamonds", "Clubs", "Spades"];
+  let values = [
+    "Ace",
+    "King",
+    "Queen",
+    "Jack",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+  ];
+
+  let res = [];
+
+  for (let card in cards) {
+    for (let value in values) {
+      res.push(values[value] + " of " + cards[card]);
+    }
+  }
+
+  res.sort(() => Math.random() - 0.5);
+
+  return res;
+}
+
+console.log(shuffleCard());
