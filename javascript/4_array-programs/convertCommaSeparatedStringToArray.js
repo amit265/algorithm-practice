3f81eec4ad4f04135c60a87c21de1777d4be6c02
Amit kumar <amitk.kumar414@gmail.com>
2026-02-04 02:18:58 +0530

    convert

---
+++ javascript/4_array-programs/convertCommaSeparatedStringToArray.js
@@ -0,0 +1,71 @@
+// JavaScript - Convert Comma Separated String To Array
+
+// 1. Using the split() Method (Most Common)
+
+{
+  const s = "apple,banana,cherry";
+  const a = s.split(",");
+  console.log(a);
+}
+
+// 2. Using Array.prototype.reduce() Method
+
+{
+  const s = "apple,banana,cherry";
+  const a = s.split("").reduce(
+    (obj, char) => {
+      if (char === ",") {
+        obj.push("");
+      } else {
+        obj[obj.length - 1] += char;
+      }
+      return obj;
+    },
+    [""],
+  );
+
+  console.log(a);
+}
+
+// 3. Using Loops and slice() Method
+
+{
+  const s = "apple,banana,cherry";
+  const a = [];
+  let start = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ",") {
+      a.push(s.slice(start, i));
+      start = i + 1;
+    }
+  }
+  a.push(s.slice(start));
+  console.log(a);
+}
+
+// 4. Using Regular Expressions (RegExp) and match() Method
+
+{
+  const s = "apple , banana , cherry ";
+  const a = s.match(/[^,\s]+/g);
+  console.log(a);
+}
+
+// Handling Edge Cases
+
+// Case 1: Trailing Commas
+
+{
+  const s = "apple,banana,cherry,";
+  const a = s.split(",").filter((item) => item !== "");
+  console.log(a);
+}
+
+// Case 2: Extra Spaces
+
+{
+  const s = " apple , banana , cherry ";
+  const a = s.split(",").map((item) => item.trim());
+  console.log(a);
+}
