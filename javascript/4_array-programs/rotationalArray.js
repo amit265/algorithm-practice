87229d5557ffbdafedf61f2b16c2ef5817689c0d
Amit kumar <amitk.kumar414@gmail.com>
2026-02-26 21:52:11 +0530

    using slice for rotationalarray.js

--- javascript/4_array-programs/rotationalArray.js
+++ javascript/4_array-programs/rotationalArray.js
@@ -11,8 +11,28 @@
 
   let arr1 = [1, 2, 3, 4, 5];
   let result = rotationFunction(arr1);
-  
 
   console.log("Original Array:", arr1);
   console.log("Array after rotation", result);
+}
+
+// Approach 2: Using slice() method
+
+{
+  function rotationalArray(arr) {
+    if (arr.length <= 1) {
+      return arr;
+    }
+
+    let element1 = arr[0];
+    let remainingElement = arr.slice(1);
+
+    return [...remainingElement, element1];
+  }
+
+  const arr1 = [10, 20, 30, 40, 50];
+  const result = rotationalArray(arr1);
+
+  console.log("Original Array:", arr1);
+  console.log("Array after Left Rotate by one :", result);
 }
