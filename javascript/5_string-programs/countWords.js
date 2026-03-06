71d247475e8dd387226af70bf791d2845b7897c9
Amit kumar <amitk.kumar414@gmail.com>
2026-03-06 08:10:28 +0530

    countWords

---
+++ javascript/5_string-programs/countWords.js
@@ -0,0 +1,10 @@
+// 1. Count Words of a String
+
+// 1. Using split() Method
+
+{
+  const count = (s) => s.trim().split(/\s+/).length;
+
+  const s = "Hello, this is a simple test";
+  console.log(count(s));
+}
