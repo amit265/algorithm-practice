fe712ebc61e553a6058e84aacbc105eb23aec4c2
Amit kumar <amitk.kumar414@gmail.com>
2026-04-06 22:37:04 +0530

    format

--- javascript/5_string-programs/formatNumberAsCurrencyString.js
+++ javascript/5_string-programs/formatNumberAsCurrencyString.js
@@ -5,3 +5,25 @@
 .format(monetary_value);  
 
 */
+
+{
+  let format = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+    minimumFractionDigits: 2,
+  });
+
+  console.log(format.format(4500));
+}
+
+
+{
+  let format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  });
+
+  console.log(format.format(40));
+}
+
