f587c1a5bcd7d07cc5cb849a0fd84f5775cb161b
Amit kumar <amitk.kumar414@gmail.com>
2026-04-27 23:53:18 +0530

    convert to float

--- javascript/5_string-programs/convert_to_float.js
+++ javascript/5_string-programs/convert_to_float.js
@@ -13,3 +13,24 @@
   n = convert_to_float(n);
   console.log("Converted value = " + n + " Type of " + n + " = " + typeof n);
 }
+
+// Method 2: By using parseFloat() Method
+
+{
+
+  function convert_to_float(a) {
+    let floatValue = parseFloat(a);
+    return floatValue;
+  }
+
+  let n = "245.165";
+
+  n = convert_to_float(n);
+
+  console.log("Converted value = " + n + " Type of " + n + " = " + typeof n);
+  n = "-915.55";
+
+  n = convert_to_float(n);
+
+  console.log("Converted value = " + n + " Type of " + n + " = " + typeof n);
+}
