8d62d436bd6a078cfb7480d4f817400a6fbc2476
Amit kumar <amitk.kumar414@gmail.com>
2026-05-06 21:57:22 +0530

    <br>

---
+++ javascript/5_string-programs/splitMultiline.js
@@ -0,0 +1,13 @@
+// 31. Split Multiline String into Array of Lines
+
+{
+  function myFunction() {
+    let string = "Are you ready?" + "<br>So let's get started";
+    return string.split("<br>");
+  }
+
+  console.log(myFunction());
+}
+
+
+
