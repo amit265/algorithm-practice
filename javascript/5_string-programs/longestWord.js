11e54ca46e6246c3e188b88485edd58c06704fcd
Amit kumar <amitk.kumar414@gmail.com>
2026-05-08 22:43:10 +0530

    longestWord

--- javascript/5_string-programs/longestWord.js
+++ javascript/5_string-programs/longestWord.js
@@ -1,4 +1,23 @@
 // 32. Find the Longest Word Within the String
 
+// Approach 1: Using regex and for...loop
 
+{
+  function longest(str) {
+    str = str.match(/[a-zA-Z0-9]+/gi);
+    let largest = "";
+    for (let i = 0; i < str.length; i++) {
+      if (str[i].length > largest.length) {
+        largest = str[i];
+      }
+    }
 
+    return largest;
+  }
+
+  console.log(
+    longest(
+      "Hello guys this is geeksforgeeks where" + " students learn programming",
+    ),
+  );
+}
