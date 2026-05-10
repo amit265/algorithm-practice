7d5d864eb0f2acfe824ed549972c9179b4e04a76
Amit kumar <amitk.kumar414@gmail.com>
2026-05-10 22:59:48 +0530

    4

--- javascript/5_string-programs/longestWord.js
+++ javascript/5_string-programs/longestWord.js
@@ -57,5 +57,26 @@
   );
 }
 
+//  Approach 4: Using split() and for...loop
 
-// 
+{
+
+  let longestWord = (string) => {
+    let stringg = string.split(" ");
+    let longest = 0;
+    let longest_word = null;
+    for (let i = 0; i < stringg.length; i++) {
+      if (longest < stringg[i].length) {
+        longest = stringg[i].length;
+        longest_word = stringg[i];
+      }
+    }
+    return longest_word;
+  };
+
+  console.log(
+    longestWord(
+      "Hello guys this is geeksforgeeks where students learn programming",
+    ),
+  );
+}
