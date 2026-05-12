0f8dc8b2472000e59cf4b8629ae1b87061077757
Amit kumar <amitk.kumar414@gmail.com>
2026-05-12 23:31:23 +0530

    3

--- javascript/5_string-programs/nthoccurrence.js
+++ javascript/5_string-programs/nthoccurrence.js
@@ -34,37 +34,80 @@
 
 // Approach 2: Using indexOf() method
 
-// Input string
-let string = "Geeks gfg Geeks Geek Geeks gfg";
+{
+  // Input string
+  let string = "Geeks gfg Geeks Geek Geeks gfg";
 
-// String to search
-let searchString = "Geeks";
+  // String to search
+  let searchString = "Geeks";
 
-// occurrence number
-let occurrence = 3;
-console.log(
-  occurrence + "rd occurrence of a '" + searchString + "' in " + string + "'.",
-);
+  // occurrence number
+  let occurrence = 3;
+  console.log(
+    occurrence +
+      "rd occurrence of a '" +
+      searchString +
+      "' in " +
+      string +
+      "'.",
+  );
 
-// Function to get index of occurrence
-function getIndex(str, substr, ind) {
-  let Len = str.length,
-    i = -1;
-  while (ind-- && i++ < Len) {
-    i = str.indexOf(substr, i);
-    if (i < 0) break;
+  // Function to get index of occurrence
+  function getIndex(str, substr, ind) {
+    let Len = str.length,
+      i = -1;
+    while (ind-- && i++ < Len) {
+      i = str.indexOf(substr, i);
+      if (i < 0) break;
+    }
+    return i;
   }
-  return i;
+
+  function GFG_Fun() {
+    console.log(getIndex(string, searchString, occurrence));
+  }
+
+  GFG_Fun();
 }
-
-function GFG_Fun() {
-  console.log(getIndex(string, searchString, occurrence));
-}
-
-GFG_Fun();
-
-
 
 // Approach 3: Using regular expressions
 
+{
+  // Input string
+  let string = "Geeks gfg Geeks Geek Geeks gfg";
 
+  // String to search
+  let searchString = "Geeks";
+
+  // occurrence number
+  let occurrence = 3;
+
+  // Create a regular expression with global flag to find all occurrences
+  let regex = new RegExp(searchString, "g");
+  let matches = Array.from(string.matchAll(regex));
+
+  // Check if nth occurrence exists
+  if (matches.length >= occurrence) {
+    let nthIndex = matches[occurrence - 1].index;
+    console.log(
+      occurrence +
+        "rd occurrence of a '" +
+        searchString +
+        "' in " +
+        string +
+        "'.",
+    );
+    console.log("Index of the " + occurrence + "rd occurrence: " + nthIndex);
+  } else {
+    console.log(
+      "The string does not have " +
+        occurrence +
+        " occurrences of '" +
+        searchString +
+        "'.",
+    );
+  }
+}
+
+
+// 
