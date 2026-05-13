ed88b7ee2bf74734c3ed13f192588ee895099082
Amit kumar <amitk.kumar414@gmail.com>
2026-05-13 23:39:47 +0530

    replace method

---
+++ javascript/5_string-programs/replace.js
@@ -0,0 +1,12 @@
+// 34. Replace All Dots in a String
+
+// Using JavaScript replace() Method
+
+{
+  let str = "A.computer.Science.Portal";
+  let res = str.replace(/\./g, " ");
+  console.log("String 1: " + str);
+  console.log("String 2: " + res);
+}
+
+
