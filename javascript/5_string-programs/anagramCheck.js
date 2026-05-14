ceb35088716d6126dcf36938aed48c330da16a2f
Amit kumar <amitk.kumar414@gmail.com>
2026-05-14 22:56:30 +0530

    using loop

--- javascript/5_string-programs/anagramCheck.js
+++ javascript/5_string-programs/anagramCheck.js
@@ -19,10 +19,45 @@
   console.log(checkAnagram("amit", "tima"));
 }
 
-
-
+// Approach 2: Using for loop
 
 {
+  function checkAnagram(a, b) {
+    let array = {};
+    if (a === b) return true;
+    if (a.length !== b.length) return false;
 
+    let minCharCode = Math.min(getMinCharCode(a), getMinCharCode(b));
+
+    for (let i = 0; i < a.length; i++) {
+      let res = a.charCodeAt(i) - minCharCode;
+      array[res] = (array[res] || 0) + 1;
+    }
+
+    for (let j = 0; j < b.length; j++) {
+      let res = b.charCodeAt(j) - minCharCode;
+      if (!array[res]) return false;
+      array[res]--;
+    }
+
+    return true;
+  }
+
+  function getMinCharCode(str) {
+    let minCharCode = Infinity;
+    for (let i = 0; i < str.length; i++) {
+      let charCode = str.charCodeAt(i);
+      if (charCode < minCharCode) {
+        minCharCode = charCode;
+      }
+    }
+    return minCharCode;
+  }
+
+  console.log("Using for loop", checkAnagram("abc", "cba"));
+    console.log("Using for loop", checkAnagram("abd", "cba"));
+  console.log("Using for loop", checkAnagram("abc", "cb"));
 
 }
+
+
