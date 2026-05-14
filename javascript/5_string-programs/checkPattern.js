4c512bf375ef76c58bd6573180a97cc8e0c35de7
Amit kumar <amitk.kumar414@gmail.com>
2026-05-14 23:53:14 +0530

    checkpattern

---
+++ javascript/5_string-programs/checkPattern.js
@@ -0,0 +1,24 @@
+// 36. Check if String Follows Order Defined by Pattern
+
+
+// 1. using javascript loops
+
+{
+
+
+    function checkPattern(str, pattern) {
+
+        // lcmi -> last char max index
+        // ccmi -> current char max index
+        // ccmin -> current char min index  
+
+        let lcmi = -1, ccmi = -1, ccmin = -1;
+        let matchValue = true;
+        lcmi = lastOcc(str, pattern[0]);
+        for(i = 1; )
+
+        
+    }
+
+
+}
