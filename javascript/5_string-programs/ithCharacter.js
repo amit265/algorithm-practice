e40ec2ca7704f2b0ba7ae0550ed3113f7c30c58e
Amit kumar <amitk.kumar414@gmail.com>
2026-05-21 22:39:31 +0530

    approach 2

--- javascript/5_string-programs/ithCharacter.js
+++ javascript/5_string-programs/ithCharacter.js
@@ -41,3 +41,59 @@ After all, iterations, return the character at the 'ith' index in the final stri
     i = 8;
   console.log(find_character(n, m, i));
 }
+
+// Approach 2
+
+/* 
+
+In this approach, we calculate the ith character in a binary string after n iterations.
+It converts a decimal number M into its binary representation, finds the block number where the character is, and handles special cases when k corresponds to the root digit.
+We use bitwise operations to efficiently determine whether to flip the root digit or not.
+The result is then printed, providing the desired character within the modified binary string.
+*/
+
+{
+  function KthCharacter(m, n, k) {
+    let distance = Math.pow(2, n);
+    let Block_number = Math.floor(k / distance);
+    let remaining = k % distance;
+    let s = new Array(32).fill(0);
+    let x = 0;
+
+    while (m > 0) {
+      s[x] = m % 2;
+      m = Math.floor(m / 2);
+      x++;
+    }
+
+    let root = s[x - 1 - Block_number];
+    if (remaining == 0) {
+      console.log(root);
+      return;
+    }
+
+    let flip = true;
+    while (remaining > 1) {
+      if ((remaining & 1) > 0) {
+        flip = !flip;
+      }
+
+      remaining = remaining >> 1;
+    }
+
+    if (flip) {
+      console.log(root > 0 ? 0 : 1);
+    } else {
+      console.log(root);
+    }
+  }
+
+  let m = 5,
+    k = 5,
+    n = 3;
+
+  KthCharacter(m, n, k);
+}
+
+
+
