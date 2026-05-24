2bf67d1a386e81e0230e77ce0f769b860e901f8e
Amit kumar <amitk.kumar414@gmail.com>
2026-05-24 23:02:36 +0530

    recursion

--- javascript/5_string-programs/substringFunction.js
+++ javascript/5_string-programs/substringFunction.js
@@ -1,6 +1,25 @@
 // 42. Get All Substrings of a Given String
 
-
 // Approach 1: Using Recursive Approach
+
+{
+  function SubstringFunction(input) {
+    if (input.length === 0) {
+      return [""];
+    }
+
+    let first = input[0];
+    let restSubstrings = SubstringFunction(input.slice(1));
+
+    let substrings = restSubstrings.map((sub) => first + sub);
+
+    return [...substrings, ...restSubstrings];
+  }
+
+  let str1 = "abc";
+  let result = SubstringFunction(str1);
+  console.log(result);
+}
+
 
 
