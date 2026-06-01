208039ab528b319f5eabb34a77863b287bc3b20a
Amit kumar <amitk.kumar414@gmail.com>
2026-06-01 23:01:26 +0530

    subtractdays

--- javascript/8_date-programs/subtractDays.js
+++ javascript/8_date-programs/subtractDays.js
@@ -1,6 +1,22 @@
 // 1. Subtract Days from Date Object
 
 {
+  let today = new Date();
+  console.log("Today's date = " + today);
+
+  Date.prototype.subtractDays = function (d) {
+    this.setDate(this.getDate() - d);
+    return this;
+  };
+
+  let a = new Date();
+  a.subtractDays(365);
+
+  console.log(a);
+  console.log("amit");
+}
+
+{
   {
     let today = new Date();
     // console.log(today.getTime());
@@ -17,20 +33,4 @@
   let a = new Date();
   a.subtractDays(4);
   console.log(a);
-}
-
-{
-  let today = new Date();
-  console.log("Today's date = " + today);
-
-  Date.prototype.subtractDays = function (d) {
-    this.setDate(this.getDate() - d);
-    return this;
-  };
-
-  let a = new Date();
-  a.subtractDays(365);
-
-  console.log(a);
-  console.log("amit")
 }
